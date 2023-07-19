2af2ffe7c4eb56a786320d569f1518a419ca13ff
BhavanaVadivel <113614209+BhavanaVadivel@users.noreply.github.com>
2023-07-19 13:28:34 +0530

    Add files via upload

---
+++ Programming/TheSecretMessageDecoder/src/MessageDecoderApp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MessageDecoderApp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Character");
+		char ch = sc.next().charAt(0);
+		MessageDecoder md = new MessageDecoder();
+		System.out.println(md.decodeCharacter(ch));
+	}
+
+}
