54d45bd7d45e72c9a44c85edc493ea38d4a93569
BhavanaVadivel <113614209+BhavanaVadivel@users.noreply.github.com>
2023-12-01 09:34:43 +0530

    Delete Programming/MultiThreading directory

--- Programming/MultiThreading/src/Demo1.java
+++
@@ -1,9 +0,0 @@
-
-public class Demo1 {
-
-	public static void main(String[] args) {
-		Thread t1  = Thread.currentThread();
-		System.out.println("Current Thread = "+t1);
-	}
-
-}
--- Programming/MultiThreading/src/Demo2.java
+++
@@ -1,9 +0,0 @@
-
-public class Demo2 extends Thread{
-	public void run() {
-		Thread t2 = Thread.currentThread();
-		System.out.println(t2);
-	}
-	
-	
-}
